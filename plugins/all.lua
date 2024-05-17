256a982c5e43d6c5b0fae438e5df6e3779ee58fb
Eran Heres <eran.heres@gmail.com>
2024-05-17 23:23:28 +0300

    removing autosession
    will use the lazy persistency plugin instead

--- plugins/all.lua
+++ plugins/all.lua
@@ -1,6 +1,6 @@
 return {
   require("common.plugins.chatgpt"),
-  require("common.plugins.autosession"),
+  --require("common.plugins.autosession"),
   require("common.plugins.toggleterm"),
   require("common.plugins.which-key"),
 }
